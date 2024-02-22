e5e9c4c5c080610157b6ae12ec4f5f157c359cd3
Alexandre CAROL <E21B036Z@ubuntu.u-inf-j-c002-16>
2024-02-22 14:00:48 +0100

    First init Spring

---
+++ src/main/kotlin/com/userslikemovies/UserLikeMovies/UserLikeMoviesApplication.kt
@@ -0,0 +1,11 @@
+package com.userslikemovies.UserLikeMovies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserLikeMoviesApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserLikeMoviesApplication>(*args)
+}
---
+++ src/test/kotlin/com/userslikemovies/UserLikeMovies/UserLikeMoviesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.userslikemovies.UserLikeMovies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserLikeMoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
