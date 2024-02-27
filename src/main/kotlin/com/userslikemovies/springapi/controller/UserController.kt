5d4023846e10acc5f1ad6fe355e8af2d5055e46b
Alexandre CAROL <E21B036Z@U-INF-J-C005-26.iut-nantes.univ-nantes.prive>
2024-02-27 13:13:33 +0100

    Add create user route

--- src/main/kotlin/com/userslikemovies/springapi/controller/UserController.kt
+++ src/main/kotlin/com/userslikemovies/springapi/controller/UserController.kt
@@ -7,6 +7,7 @@ import com.userslikemovies.springapi.service.UserService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,5 +25,4 @@ class UserController(val userService: IUserService) {
     fun createUser(@RequestBody userDTO : UserDTO) {
         userService.createUser(userDTO.asUser())
     }
-
 }
