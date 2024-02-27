3c81f2d46c8713c66e9fdee432da8301e5c49bc0
E21B036Z <alexandre.carol@etu.univ-nantes.fr>
2024-02-27 21:16:18 +0100

    add swagger at /swagger-ui.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
 
 tasks.withType<KotlinCompile> {
