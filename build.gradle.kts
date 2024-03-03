36445da2073da4def794a3a3f4526a93e4a7d097
lucas <lutinelo.lg@gmail.com>
2024-03-03 16:58:05 +0100

    FunctionnalErrors

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/userslikemovies/springapi/controller/UserController.kt
+++ src/main/kotlin/com/userslikemovies/springapi/controller/UserController.kt
@@ -11,6 +11,8 @@ import com.userslikemovies.springapi.repository.IUserRepository
 import com.userslikemovies.springapi.service.IUserService
 import com.userslikemovies.springapi.service.UserService
 import jakarta.validation.constraints.Email
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -23,13 +25,17 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 
 @RestController
 @Validated
 class UserController(val userService: IUserService, private val customProperties : CustomProperties) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping("/api/v1/users")
     fun getUsers(@RequestParam age : Int?): ResponseEntity<out Any> {
+        logger.info("Get all users")
         var result = userService.getUsers(age)
         if (result.isSuccess){
             if (result.getOrNull() != null){
@@ -43,19 +49,19 @@ class UserController(val userService: IUserService, private val customProperties
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(FunctionnalError("400", "Bad Request"))
     }
 
-    //Verfier les exceptions conflits
     @PostMapping("api/v1/users")
     fun createUser(@RequestHeader apiKey : String, @RequestBody userDTO : UserCreationDTO): ResponseEntity<out Any> {
+        logger.info("Received request to get users.")
         if (apiKey != customProperties.apiKey) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized"))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized").toString())
         }
         var result = userService.createUser(userDTO.asUser())
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is UserAlreadyExistsException){
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(FunctionnalError("409", "User already exist"))
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body(FunctionnalError("409", "User already exist").toString())
                 }else{
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(FunctionnalError("400", "Bad Request"))
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(FunctionnalError("400", "Bad Request").toString())
                 }
             }
         }
@@ -64,18 +70,17 @@ class UserController(val userService: IUserService, private val customProperties
 
     @PutMapping("api/v1/users/{email}")
     fun updateUser(@RequestHeader apiKey : String, @PathVariable email : String, @RequestBody userDTO : UserDTO): ResponseEntity<out Any> {
-        println(apiKey)
+        logger.info("Update user")
         if (apiKey != customProperties.apiKey){
-            print("45")
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized"))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized").toString())
         }
         var result = userService.updateUser(email, userDTO.asUser())
         if (result.isFailure){
             if (result.exceptionOrNull() != null) {
                 if (result.exceptionOrNull() is UserNotFoundException) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found").toString())
                 }else if (result.exceptionOrNull() is InvalidPayload){
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(FunctionnalError("400", "Invalid payload"))
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(FunctionnalError("400", "Invalid payload").toString())
                 }
             }
         }
@@ -84,14 +89,15 @@ class UserController(val userService: IUserService, private val customProperties
 
     @DeleteMapping("api/v1/users/{email}")
     fun deleteUser(@RequestHeader apiKey : String, @PathVariable email : String): ResponseEntity<out Any> {
+        logger.info("Delete user")
         if (apiKey != customProperties.apiKey) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized"))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized").toString())
         }
         val result = userService.deleteUser(email)
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is UserNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found").toString())
                 }
             }
         }
@@ -100,11 +106,12 @@ class UserController(val userService: IUserService, private val customProperties
 
     @GetMapping("api/v1/users/{email}")
     fun getUserByEmail(@PathVariable @Email email: String): ResponseEntity<out Any> {
+        logger.info("Get user by email")
         val result = userService.getUserByEmail(email)
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is UserNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found").toString())
                 }
             }
         }
@@ -113,15 +120,16 @@ class UserController(val userService: IUserService, private val customProperties
 
     @PostMapping("api/v1/users/{email}/favoriteMovies")
     fun addFavoritesMovie(@PathVariable @Email email : String, @RequestBody addMovie : AddMovieDTO): ResponseEntity<out Any> {
+        logger.info("Add a favorite movie of a user")
        var result = userService.addUserFavoriteMovie(email, addMovie.movieId)
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is MovieNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found").toString())
                 }else if (result.exceptionOrNull() is UserNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found").toString())
                 }else if (result.exceptionOrNull() is MovieAlreadyInFavorites){
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(FunctionnalError("409", "Movie already in favorites"))
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body(FunctionnalError("409", "Movie already in favorites").toString())
                 }
             }
         }
@@ -130,15 +138,16 @@ class UserController(val userService: IUserService, private val customProperties
 
     @DeleteMapping("api/v1/users/{email}/favoriteMovies/{movieId}")
     fun deletefavoriteMovie(@PathVariable @Email email : String, @PathVariable movieId : Int): ResponseEntity<out Any> {
+        logger.info("Delete a favorite movie of a user")
         var result = userService.removeUserFavoriteMovie(email, movieId)
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is UserNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "User not found").toString())
                 }else if (result.exceptionOrNull() is MovieNotInFavorites){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("400", "Movie not in favorites"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("400", "Movie not in favorites").toString())
                 }else{
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found").toString())
                 }
             }
         }
@@ -147,11 +156,12 @@ class UserController(val userService: IUserService, private val customProperties
 
     @GetMapping("api/v1/movies/{movieId}")
     fun getMoviePreferenceNumber(@PathVariable movieId : Int): ResponseEntity<out Any> {
+        logger.info("Get the number of users who have bookmarked the movie")
         var result = userService.getMoviePreferenceNumber(movieId)
         if (result.isFailure){
             if (result.exceptionOrNull() != null){
                 if (result.exceptionOrNull() is MovieNotFoundException){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found"))
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found").toString())
                 }
             }
         }
@@ -159,16 +169,17 @@ class UserController(val userService: IUserService, private val customProperties
     }
 
     @DeleteMapping("api/v1/movies/{movieId}")
-    fun movieDeleted(@RequestHeader apiKey : String, @PathVariable movieId : Int): ResponseEntity<FunctionnalError> {
+    fun movieDeleted(@RequestHeader apiKey : String, @PathVariable movieId : Int): ResponseEntity<out Any> {
+        logger.info("Delete a movie")
         if (apiKey == customProperties.apiKey){
             var result = userService.movieDeleted(movieId)
             if (result ==  null){
                 return ResponseEntity.status(HttpStatus.OK).body(result)
             }else{
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found"))
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(FunctionnalError("404", "Movie not found").toString())
             }
         }else{
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized"))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(FunctionnalError("401", "Unauthorized").toString())
         }
     }
 }
